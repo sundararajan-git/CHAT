e37840b7117bd56cc3a39a0bed45e14d05fe9b64
sundararajan-git <sundararajanselvarasu@gmail.com>
2024-12-23 22:57:32 +0530

    backend stepup

---
+++ BACKEND/SRC/ROUTES/userRoutes.js
@@ -0,0 +1,41 @@
+import express from "express";
+const router = express.Router();
+
+import {
+  signUp,
+  login,
+  logout,
+  forgotPassword,
+  resetPassword,
+  updateProfile,
+  verifyEmail,
+  isValidUser,
+} from "../CONTROLLERS/userController.js";
+
+import { verifyToken } from "../MIDDLEWARE/validUser.js";
+
+// CHECK IS VALID USER OR NOT
+router.get("/isvaliduser", verifyToken, isValidUser);
+
+// USER RIGISTER
+router.post("/signup", signUp);
+
+// USER VERIFICATION
+router.post("/verify", verifyEmail);
+
+//  USER LOGIN
+router.post("/login", login);
+
+// USER LOGOUT
+router.post("/logout", logout);
+
+// USER PROFILE UPDATE
+router.put("/updateprofile", updateProfile);
+
+// USER PASSWORD UPDATE
+router.put("/resetpassword/:token", resetPassword);
+
+// USER FORGOT PASSWORD
+router.post("/forgotpassword", forgotPassword);
+
+export default router;
--- BACKEND/SRC/Server.js
+++ BACKEND/SRC/Server.js
@@ -1,1 +1,39 @@
-console.log("Hello world")
+import dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import userRoutes from "../SRC/ROUTES/userRoutes.js";
+import { connectDB } from "./DB/connectDB.js";
+
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT;
+
+//  MIDDLEWEARES
+const corsOptions = {
+  origin: "http://localhost:5173",
+  Credential: true,
+  optionsSuccessStatus: 200,
+};
+
+// ALLOWED ORIGINS
+app.use(cors(corsOptions));
+
+// ALLOW THE JSON DATA
+app.use(express.json());
+
+// ENABLE COOKIES PARSING
+app.use(cookieParser());
+
+// USER ROUTES
+app.use('api/users', userRoutes);
+
+//  CHATS ROUTES
+// app.use("api/chat", () => {});
+
+app.listen(PORT, () => {
+  connectDB();
+  console.log("Server is running on " + PORT);
+});
